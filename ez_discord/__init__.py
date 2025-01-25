12a63403c1bc83c4efdf1dd28ffe67ad6ae019f8
Kaede Dev Kento Hinode <139364723+DarsheeeGamer@users.noreply.github.com>
2025-01-25 14:36:34 +0530

    Create __init__.py

---
+++ ez_discord/__init__.py
@@ -0,0 +1,8 @@
+# ez_discord/__init__.py
+
+"""
+EzDiscord - A Python Discord API Wrapper
+
+This package provides a Python wrapper for the Discord API,
+making it easier to interact with Discord's functionalities.
+"""
